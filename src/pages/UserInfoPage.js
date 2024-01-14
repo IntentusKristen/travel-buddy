a582e0924703985d76773f8712bf6794ccbc0db7
slee2769 <slee2769@uwo.ca>
2024-01-14 01:13:14 -0500

    input from user displays on screen

--- src/pages/UserInfoPage.js
+++ src/pages/UserInfoPage.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../UserInfoPage.css';
 
+
 const UserInfoPage = () => {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [emergencyContact, setEmergencyContact] = useState('');
+  const [transportationOne, setTransportationOne] = useState('');
+  const [transportationTwo, setTransportationTwo] = useState('');
+  const [transportationThree, setTransportationThree] = useState('');
+  const [transportationFour, setTransportationFour] = useState('');
+  const[isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    setIsSubmitted(true);
+  };
+  
   return (
     <div class = "bigDiv">
       <div class = "topDiv"> 
@@ -12,19 +28,19 @@ const UserInfoPage = () => {
       <div class = "infoDiv">
       <form>
         <label for = "fname" class = "firstName"> First name </label>
-        <input type = "text" name = "fname" id = "fname"/>
+        <input type = "text" name = "fname" id = "fname" value = {firstName} onChange={ (e) => setFirstName(e.target.value)}/> <br></br>
 
         <label for = "lname" class = "lastName"> Last name </label>
-        <input type = "text" name = "lname" id = "lname"/> <br></br>
+        <input type = "text" name = "lname" id = "lname" value = {lastName} onChange={ (e) => setLastName(e.target.value)}/> <br></br>
 
         <label for = "pnumber" class = "phoneNumber"> Phone number </label>
-        <input type = "tel" name = "pnumber" id = "pnumber"/><br></br>
-        Emergency contact: <input type = "tel" name = "econtact" id = "econtact"/>
+        <input type = "tel" name = "pnumber" id = "pnumber" value = {phoneNumber} onChange={ (e) => setPhoneNumber(e.target.value)}/><br></br>
+        Emergency contact: <input type = "tel" name = "econtact" id = "econtact" value = {emergencyContact} onChange={ (e) => setEmergencyContact(e.target.value)}/>
 
         <p> Rank your transportation preference </p>
 
         <label for="transportation-methods" class = "first"> First </label> 
-        <select name="transportation-methods" id="transportation-methods"> 
+        <select name="transportation-methods" id="transportation-methods" value = {transportationOne} onChange={ (e) => setTransportationOne(e.target.value)}> 
         <option value="" disabled selected>Select an option</option>
         <option value="walking">Walking</option> 
         <option value="bussing">Bussing</option> 
@@ -34,7 +50,7 @@ const UserInfoPage = () => {
 
         <br></br>
         <label for="transportation-methods"> Second </label> 
-        <select name="transportation-methods" id="transportation-methods"> 
+        <select name="transportation-methods" id="transportation-methods" value = {transportationTwo} onChange={ (e) => setTransportationTwo(e.target.value)}> 
         <option value="" disabled selected>Select an option</option>
         <option value="walking">Walking</option> 
         <option value="bussing">Bussing</option> 
@@ -44,7 +60,7 @@ const UserInfoPage = () => {
 
         <br></br>
         <label for="transportation-methods" class = "third"> Third </label> 
-        <select name="transportation-methods" id="transportation-methods"> 
+        <select name="transportation-methods" id="transportation-methods" value = {transportationThree} onChange={ (e) => setTransportationThree(e.target.value)}> 
         <option value="" disabled selected>Select an option</option>
         <option value="walking">Walking</option> 
         <option value="bussing">Bussing</option> 
@@ -54,7 +70,7 @@ const UserInfoPage = () => {
 
         <br></br>
         <label for="transportation-methods" class = "fourth"> Fourth </label> 
-        <select name="transportation-methods" id="transportation-methods"> 
+        <select name="transportation-methods" id="transportation-methods" value = {transportationFour} onChange={ (e) => setTransportationFour(e.target.value)}> 
         <option value="" disabled selected>Select an option</option>
         <option value="walking">Walking</option> 
         <option value="bussing">Bussing</option> 
@@ -63,10 +79,20 @@ const UserInfoPage = () => {
         </select>
         <br></br>
   
-        <button type="submit" class = "button">Confirm</button>
+        <button type="button" class = "button" onClick={handleSubmit}>Confirm</button>
       </form>
         
       </div>
+
+      <div id="displayArea">
+        {isSubmitted && firstName && lastName && <p> Hi, {firstName} {lastName} </p>}
+
+        {isSubmitted && phoneNumber && emergencyContact && <p> Welcome to travel buddy! We will store your phone number {phoneNumber} to send text updates and keep the number {emergencyContact} for emergency contact purposes only</p>}
+
+        {isSubmitted && transportationOne && transportationTwo && transportationThree && transportationFour && <p> You have selected your transportation priority in the following order: {transportationOne}, {transportationTwo}, {transportationThree} and {transportationFour}</p>}
+
+      </div>
+
     </div>
   )
 
